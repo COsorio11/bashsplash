48be947dd879dcbf136b91db4f95017db933e987
localadmin <localadmin@compozed-tss-02.allstate.com>
2016-11-04 10:11:05 -0700

    init commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for script in ./*.sh
+do
+  if [ ! $script == ./install.sh ]; then
+    echo "Installing $script"
+    # setting file equal to filename without extension. dot is there as it is removed in the cut but is necessary
+    FILE=.$(echo $script | cut -f 2 -d '.')
+    mv $script $FILE
+    ln -s $FILE /usr/local/bin
+    echo "Install Successful"
+  fi
+done
